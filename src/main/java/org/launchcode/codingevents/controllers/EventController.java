33f9f98f8e50878975ce564d5fbc913acebd5606
krgirard33 <ken.girard@gmail.com>
2023-02-13 23:28:45 -0600

    Changed events to HashMap, displays description & address, can create new events

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -7,16 +7,22 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.ArrayList;
-import java.util.List;
+// import java.util.ArrayList;
+import java.util.HashMap;
+// import java.util.List;
 
 @Controller
 @RequestMapping("events")
 public class EventController {
 
-    private static List<String> events = new ArrayList<>();
+    private static HashMap<String, String> events = new HashMap<>();
+
+    // private static List<String> events = new ArrayList<>();
     @GetMapping
     public String displayAllEvents(Model model) {
+        events.put("Menteaship","A fun meetup for connecting with mentors");
+        events.put("Code With Pride","A fun meetup sponsored by LaunchCode");
+        events.put("Javascripty", "An imaginary meetup for Javascript developers");
         model.addAttribute("events", events);
         return "events/index";
     }
@@ -29,8 +35,8 @@ public class EventController {
 
     // lives at events/create
     @PostMapping("create")
-    public String createEvent(@RequestParam String eventName) {
-        events.add(eventName);
+    public String createEvent(@RequestParam String eventName, @RequestParam String eventDescription) {
+        events.put(eventName, eventDescription);
         return "redirect:/events";
     }
 }
