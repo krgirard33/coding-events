2cafc70f257006b1f34e690631469adc1c85c73b
krgirard33 <ken.girard@gmail.com>
2023-03-04 12:55:22 -0600

    Created edit.html, displayEditForm, processEditForm, added link to edit on index.html

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -58,4 +58,30 @@ public class EventController {
         }
         return "redirect:/events";
     }
+
+    @GetMapping("edit/{eventId}")
+    public String displayEditForm(Model model, @PathVariable int eventId, @ModelAttribute Event newEvent) {
+        Event eventToEdit = EventData.getById(eventId);
+        System.out.println("************************************ "+eventToEdit.getId()+" **********************************");
+        model.addAttribute("eventId", eventId);
+        String title = "Edit Event "+eventToEdit.getName()+" (id="+eventToEdit.getId()+")";
+        model.addAttribute("title",title);
+        model.addAttribute("types",EventType.values());
+        model.addAttribute("event", eventToEdit);
+        return "events/edit";
+    }
+
+    @PostMapping("edit")
+    public String processEditForm(int eventId, String name, String description, String contactEmail, EventType type) {
+//        System.out.println("WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW");
+        System.out.println("caw caw caw "+eventId+" "+name+" "+type+" caw caw caw");
+
+      Event eventToEdit = EventData.getById(eventId);
+//        System.out.println("``````````````````````````````"+eventToEdit.getId()+"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        eventToEdit.setName(name);
+        eventToEdit.setDescription(description);
+        eventToEdit.setContactEmail(contactEmail);
+//        eventToEdit.setType(type);
+        return "redirect:";
+    }
 }
