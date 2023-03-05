f8e37e307ba79285c576e4143b9731eab88f3f00
krgirard33 <ken.girard@gmail.com>
2023-03-04 21:26:57 -0600

    Completed the 4.1 Database Configuration video

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -24,7 +24,7 @@ public class EventController {
     @GetMapping("create")
     public String displayCreateEventForm(Model model) {
         model.addAttribute("title","Create Event");
-        model.addAttribute("event", new Event());
+        model.addAttribute(new Event());
         model.addAttribute("types",EventType.values());
         return "events/create";
     }
@@ -59,29 +59,29 @@ public class EventController {
         return "redirect:/events";
     }
 
-    @GetMapping("edit/{eventId}")
-    public String displayEditForm(Model model, @PathVariable int eventId, @ModelAttribute Event newEvent) {
-        Event eventToEdit = EventData.getById(eventId);
-        System.out.println("************************************ "+eventToEdit.getId()+" **********************************");
-        model.addAttribute("eventId", eventId);
-        String title = "Edit Event "+eventToEdit.getName()+" (id="+eventToEdit.getId()+")";
-        model.addAttribute("title",title);
-        model.addAttribute("types",EventType.values());
-        model.addAttribute("event", eventToEdit);
-        return "events/edit";
-    }
-
-    @PostMapping("edit")
-    public String processEditForm(int eventId, String name, String description, String contactEmail, EventType type) {
-//        System.out.println("WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW");
-        System.out.println("caw caw caw "+eventId+" "+name+" "+type+" caw caw caw");
-
-      Event eventToEdit = EventData.getById(eventId);
-//        System.out.println("``````````````````````````````"+eventToEdit.getId()+"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-        eventToEdit.setName(name);
-        eventToEdit.setDescription(description);
-        eventToEdit.setContactEmail(contactEmail);
-//        eventToEdit.setType(type);
-        return "redirect:";
-    }
+//    @GetMapping("edit/{eventId}")
+//    public String displayEditForm(Model model, @PathVariable int eventId, @ModelAttribute Event newEvent) {
+//        Event eventToEdit = EventData.getById(eventId);
+//        //System.out.println("************************************ "+eventToEdit.getId()+" **********************************");
+//        model.addAttribute("event", eventToEdit);
+//        String title = "Edit Event "+eventToEdit.getName()+" (id="+eventToEdit.getId()+")";
+//        model.addAttribute("title",title);
+//        model.addAttribute("types",EventType.values());
+//        //model.addAttribute("event", eventToEdit);
+//        return "events/edit";
+//    }
+//
+//    @PostMapping("edit")
+//    public String processEditForm(int eventId, String name, String description, String contactEmail, EventType type) {
+////        System.out.println("WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW");
+////        System.out.println("caw caw caw "+eventId+" "+name+" "+type+" caw caw caw");
+//
+//      Event eventToEdit = EventData.getById(eventId);
+////        System.out.println("``````````````````````````````"+eventToEdit.getId()+"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+//        eventToEdit.setName(name);
+//        eventToEdit.setDescription(description);
+//        eventToEdit.setContactEmail(contactEmail);
+////        eventToEdit.setType(type);
+//        return "redirect:";
+//    }
 }
--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -14,7 +14,7 @@ public class Event {
     @Size(min = 3, max = 50, message = "Name must be between 3-50 characters")
     private String name;
 
-    @Size(max = 500, message = "Description to long.")
+    @Size(max = 500, message = "Description too long.")
     private String description;
 
     @NotBlank(message="Email required.")
